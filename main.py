74743a7641067ca922b3819d744a5fab958759ec
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-01-03 19:49:13 -0500

    Added dosage table

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ tk = tkinter.Tk()
 tk.title("US alcohol units calculator")
 tk.iconphoto(True, tkinter.PhotoImage(file=os.path.join(os.path.dirname(__file__), "icon.png")))
 
-# Configuure the rows and columns
-for row in range(3):
+for row in range(5):
     tk.rowconfigure(row, weight=1)
 for col in range(3):
     tk.columnconfigure(col, weight=1) 
@@ -53,5 +52,21 @@ def calculate():
 calculate_btn = tkmacosx.Button(tk, text="Calculate", command=calculate)
 calculate_btn.configure(bg="#007AFF", fg="white")
 calculate_btn.grid(row=2, column=0, columnspan=3)
+
+# Display a grid displaying the alcohol dosage according to psychonautwiki
+dosage_label = tkinter.Label(tk, text="Alcohol dosage (units)")
+dosage_label.grid(row=3, column=0, columnspan=3)
+
+dosage_table = tkinter.Listbox(tk)
+dosage_table.grid(row=4, column=0, columnspan=3)
+# Set the dosage table items to be unselectable and to display 5 items
+dosage_table.config(selectmode="none", height=5)
+
+dosage_table.insert(tkinter.END, "Threshold: 1 standard drink")
+dosage_table.insert(tkinter.END, "Light: 1-3 standard drinks")
+dosage_table.insert(tkinter.END, "Common: 3-5 standard drinks")
+dosage_table.insert(tkinter.END, "Strong: 5-6 standard drinks")
+dosage_table.insert(tkinter.END, "Heavy: 6+ standard drinks")
+
 
 tk.mainloop()
