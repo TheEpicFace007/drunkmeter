92a44d3d603d02947fb912f6cc6f051df1646339
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-01-04 17:05:15 -0500

    Added splashscreen

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-import tkinter
-import tkmacosx
 import os
+import random
+import sys
+import threading
+import time
+import tkinter
 import tkinter.messagebox
+import tkinter.ttk as ttk
+
+import tkmacosx
+import ttkthemes
+from PIL import Image
+
+import splashscreen
+
+THEME = "Clearlooks"
+
 
 def calculate_standard_drink(abv, volume):
+    """Calculate the number of standard drink in a drink"""
     return round(abv * volume / 1000, 2)
 
-tk = tkinter.Tk()
-tk.title("Drunkmeter")
-tk.iconphoto(True, tkinter.PhotoImage(file=os.path.join(os.path.dirname(__file__), "icon.png")))
+
+splash = splashscreen.SplashScreen(Image.open(os.path.join("splashscreen.png")),
+                                   most_common_color_idx=random.randint(0, 4),
+                                   theme=THEME,
+                                   )
+
+tk = ttkthemes.ThemedTk(theme=THEME, baseName="Drunkmeter")
+# Set the file icon.png as the icon of the window
+splash.tk.call('wm', 'iconphoto', splash._w, tkinter.PhotoImage(file=os.path.join("icon.png")))
 
 for row in range(5):
     tk.rowconfigure(row, weight=1)
 for col in range(3):
-    tk.columnconfigure(col, weight=1) 
-
-# Setup a menubar
-menu = tkinter.Menu(tk)
-tk.config(menu=menu)
-
-# Add a file menu
-file_menu = tkinter.Menu(menu)
-file_menu.add_command(label="Exit", command=tk.destroy)
+    tk.columnconfigure(col, weight=1)
 
 abv_label = tkinter.Label(tk, text="ABV (%)")
 abv_label.grid(row=0, column=0)
@@ -41,28 +53,34 @@ vol_entry.grid(row=1, column=1)
 result_label = tkinter.Label(tk, text="Result: ")
 result_label.grid(row=1, column=2)
 
-def calculate():  
+def calculate():
     try:
         if float(abv_var.get()) >= 100:
-            tkinter.messagebox.showerror("Error", "ABV must be less than or equal to 100%", icon="error", parent=tk)
+            tkinter.messagebox.showerror(
+                "Error", "ABV must be less than 100%", icon="error", parent=tk)
             return
         elif float(abv_var.get()) <= 0:
-            tkinter.messagebox.showerror("Error", "ABV must be greater than 0%", icon="error", parent=tk)
-        
+            tkinter.messagebox.showerror(
+                "Error", "ABV must be greater than 0%", icon="error", parent=tk)
+
         if float(vol_var.get()) <= 0:
-            tkinter.messagebox.showerror("Error", "Volume must be greater than 0", icon="error", parent=tk)
-            return  
-        standard_drinks = calculate_standard_drink(float(abv_var.get()), float(vol_var.get()))
+            tkinter.messagebox.showerror(
+                "Error", "Volume must be greater than 0", icon="error", parent=tk)
+            return
+        standard_drinks = calculate_standard_drink(
+            float(abv_var.get()), float(vol_var.get()))
         result = f"{standard_drinks} standard drink{standard_drinks > 1 and 's' or ''}"
     except ValueError as e:
         # tkinter.messagebox.showerror("Error", str(e), icon="error", parent=tk)
         result = "Invalid input"
-    
+
     result_label.config(text="Result: {}".format(result))
 
+
 calculate_btn = tkmacosx.Button(tk, text="Calculate", command=calculate)
-calculate_btn.configure(bg="#FFC000", fg="black", activebackground="#ffd558", activeforeground="black")
-calculate_btn.grid(row=2, column=0, columnspan=3)
+calculate_btn.configure(bg="#FFC000", fg="black",
+                        activebackground="#ffd558", activeforeground="black")
+calculate_btn.grid(row=2, column=0, columnspan=3, padx=10)
 
 # Display a grid displaying the alcohol dosage according to psychonautwiki
 dosage_label = tkinter.Label(tk, text="Alcohol dosage (units)")
@@ -79,5 +97,26 @@ dosage_table.insert(tkinter.END, "Common: 3-5 standard drinks")
 dosage_table.insert(tkinter.END, "Strong: 5-6 standard drinks")
 dosage_table.insert(tkinter.END, "Heavy: 6+ standard drinks")
 
+loading_texts = ["Loading wines", "Opening cans of beers",
+                 "Loading spirits", "Mixing cocktails", "Pouring shots"]
 
-tk.mainloop()
+def loading_thread():
+    # Focus to the loading window
+    splash.focus_force()
+    if sys.platform in ['win32', 'cygwin']:
+        tk.wm_attributes("-disabled", True)
+    time.sleep(0.5)
+    for i, text in enumerate(loading_texts):
+        splash.loading_status = text
+        time.sleep(0.5 * i)
+    
+    if sys.platform in ['win32', 'cygwin']:
+        tk.wm_attributes("-disabled", False)
+    splash.destroy()
+
+# Center the tk window
+tk.eval('tk::PlaceWindow %s center' % tk.winfo_pathname(tk.winfo_id()))
+
+splash.loading_status = "Loading..."
+threading.Thread(target=loading_thread).start()
+splash.mainloop()
