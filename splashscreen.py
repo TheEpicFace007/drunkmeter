46afa211e0023280e81978064e5bb8e610d8d388
Charlie <37189791+TheEpicFace007@users.noreply.github.com>
2023-01-05 02:49:43 -0500

    Simplified the splashscreen

--- splashscreen.py
+++ splashscreen.py
@@ -32,72 +32,37 @@ def is_color_dark(color):
         color = tuple(int(color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
     return sum(color) < 382
 
-class SplashScreen(ttkthemes.ThemedTk, mtTkinter.Toplevel):
-    def __init__(self, splash_img, most_common_color_idx=0, theme=None, *args, **kwargs):
-        super(SplashScreen, self).__init__(theme=theme, *args, **kwargs)
+class SplashScreen(tkinter.Toplevel):
+    def __init__(self, splash_img, root, most_common_color_idx=0, theme=None, *args, **kwargs):
+        super(SplashScreen, self).__init__(root, *args, **kwargs)
         splash_img.thumbnail((600, 400))
-        self.wm_attributes("-topmost", True)
 
+        self.wm_attributes("-topmost", True)
         if sys.platform == "darwin":
             self.wm_attributes("-transparent", True)
             self.wm_attributes("-notify", True)
             self.wm_attributes("-fullscreen", False)
         elif sys.platform in ["win32", "cygwin"]:
             self.wm_attributes("-toolwindow", True)
-            
         
         # Set the image
-        self._gui_image_container = tkinter.Label(self)
-        self._gui_image_container.image = ImageTk.PhotoImage(splash_img)
-        self._gui_image_container.configure(image=self._gui_image_container.image)
-        self._gui_image_container.pack(side='top', fill="both", expand="yes")
-        self.geometry(f"{splash_img.width}x{splash_img.height+30}")
+        self.splash_img = splash_img
+        self.splash_img = ImageTk.PhotoImage(self.splash_img)
+        self._gui_image_container = tkinter.Label(self, image=self.splash_img)
+        self._gui_image_container.config(image=self.splash_img)
+        
+        self._gui_image_container.pack(fill="both", expand="yes")
+        self.geometry(f"{splash_img.width}x{splash_img.height}")
 
-        # loading info label
-        self._loading_label = tkinter.Label(self, text="")
-        self._loading_label.config(font=("Helvetica", 20, "bold"), 
-                                   fg=is_color_dark(get_5_most_common_color(splash_img)[most_common_color_idx]) and 'white' or 'black',
-                                   bg=colortuple_to_hex(get_5_most_common_color(splash_img)[most_common_color_idx]), 
-                                      borderwidth=0
-                                   )
-        self._loading_label.pack(fill="x", expand="yes")
-        # loading bar 
-        self.loading_bar = ttk.Progressbar(self, orient='horizontal', length=600, mode="indeterminate")
-        self.loading_bar.pack(side='bottom', fill="x", expand="yes")
-        self.loading_bar.start()
         # Set sizing constraints
-        self.geometry(f"{splash_img.width}x{splash_img.height+(sys.platform in ['win32', 'cygwin'] and 50 or 40)}")
+        self.geometry(f"{splash_img.width}x{splash_img.height}")
         self.resizable(False, False)
         # Put the window on the center of the screen
-        self.eval('tk::PlaceWindow . center')
+        self.update_idletasks()
+        self.geometry(f"+{int(self.winfo_screenwidth()/2 - self.winfo_width()/2)}+{int(self.winfo_screenheight()/2 - self.winfo_height()/2)}")
+        self.after(3000, self.destroy)
         
-    @property
-    def loading_status(self):
-        return self._loading_label['text']
-    
-    @loading_status.setter
-    def loading_status(self, value):
-        self._loading_label['text'] = value
-    
-    def destroy(self):
-        self.loading_bar.stop()
-        return super(SplashScreen, self).destroy()
-
-    def __delattr__(self, __name: str) -> None:
-        return super().__delattr__(__name)
-    
 if __name__ == '__main__':
     splash_img = Image.open("./splashscreen.png")
-    print(get_5_most_common_color(splash_img))
-    
-    splashscreen = SplashScreen(splash_img, most_common_color_idx=random.randint(0, 4))
-
-    def after_few_seconds():
-        import time
-        for i in range(10):
-            time.sleep(1)
-            print(f"Loading... {i}")
-            splashscreen.loading_status = f"Loading... {i}"
-    threading.Thread(target=after_few_seconds).start()
+    splashscreen = SplashScreen(splash_img, None, most_common_color_idx=random.randint(0, 4))
     splashscreen.mainloop()
-  
